4df44b6fc55efe91d7c892b272daf7596761adbd
Bretton Wade <brettonw.wade@gmail.com>
2017-12-14 09:15:09 -0500

    updating to newer bedrock

--- index.js
+++ index.js
@@ -102,62 +102,10 @@ Http.get ("images-subset.json?" + now, function (records) {
     let headerLineElement = Html.addElement (bedrockDatabaseDisplay, "div", { id: "bedrock-header-line" });
     for (let key of fieldKeys) {
         let headerElement = Html.addElement (headerLineElement, "div", { class: "bedrock-header-entry" });
-        Html.addElement (headerElement, "div", { classes: [ makeStyleNameFromColumnName (key), "bedrock-header-entry-text" ] }).innerHTML = key;
+        Html.addElement (headerElement, "div", { classes: [key.replace (/ /g, "-").toLowerCase (), "bedrock-header-entry-text" ] }).innerHTML = key;
     }
     let bedrockDatabaseDisplayList = Html.addElement (bedrockDatabaseDisplay, "div", { id:"bedrock-database-display-list", onscroll: function (event) { scrollView (event.srcElement); } });
 
-    let scrolledIntoView = function (element, list) {
-        let height = list.clientHeight;
-        let halfHeight = height / 2;
-        let coords = element.getBoundingClientRect ();
-        return (coords.bottom >= -halfHeight) && (coords.top <= (height + halfHeight));
-    };
-
-    // deal with displaying the database
-    let currentDb;
-    let recordCount;
-    const displayLineSize = 64;
-    const pageSize = Math.floor (bedrockDatabaseDisplayList.offsetHeight / displayLineSize) * 2;
-    const pageHeight = displayLineSize * pageSize;
-    let pageCount;
-
-    let pageIsInView = function (page, view) {
-        let pageInfo = page.id.split (/-/);
-        let start = (parseInt (pageInfo[1]) * displayLineSize) - view.scrollTop;
-        let end = (parseInt (pageInfo[2]) * displayLineSize) - view.scrollTop;
-        return (end >= 0) && (start <= view.clientHeight);
-    };
-
-    let lastVisiblePages = [];
-
-    let scrollView = function (view) {
-        let visibleViews = [];
-
-        // clear the lastVisiblePages
-        for (let page of lastVisiblePages) {
-            if (pageIsInView (page, view)) {
-                visibleViews.push (page);
-            } else {
-                Html.removeAllChildren (page);
-            }
-        }
-
-        // figure out which pages to make visible
-        let start = Math.floor (view.scrollTop / pageHeight);
-        let end = Math.min (pageCount, start + 2);
-        let pages = view.children;
-        for (let page of pages) {
-            if (pageIsInView (page, view)) {
-                visibleViews.push (page);
-                if (page.children.length == 0) {
-                    populatePage (page);
-                }
-            }
-        }
-
-        // reset the visible pages for the next time
-        lastVisiblePages = visibleViews;
-    };
 
     // function to populate a page
     let populatePage = function (pageElement) {
@@ -208,27 +156,8 @@ Http.get ("images-subset.json?" + now, function (records) {
     // build the database filter
     Bedrock.Database.Container.new ({
         database: records,
-        filterValues: [{ "field": "Type" }],
         onUpdate: function (db) {
-            // reset everything
-            currentDb = db;
-            bedrockDatabaseDisplayList.scrollTop = 0;
-
-            Html.removeAllChildren (bedrockDatabaseDisplayList);
-
-            // build out the paging flow, computing the page height such that it would be impossible to have more than 2
-            // pages visible at any one time
-            recordCount = currentDb.length;
-            pageCount = Math.floor (recordCount / pageSize) + (((recordCount % pageSize) > 0) ? 1 : 0);
-
-            // loop over all of the records, page by page
-            for (let pageIndex = 0; pageIndex < pageCount; ++pageIndex) {
-                let start = pageIndex * pageSize;
-                let end = Math.min(start + pageSize, recordCount);
-                let count = end - start;
-                Html.addElement (bedrockDatabaseDisplayList, "div", { id: "page-" + start + "-" + end, style: { height: (count * displayLineSize) + "px" } });
-            }
-            scrollView (bedrockDatabaseDisplayList);
+            Bedrock.PagedDisplay.makeTable (bedrockDatabaseDisplayList, db, fields);
 
             // save the result as a CSV file
             /*
